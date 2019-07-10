4c03b3b2f28d2d9a735ddd271781b9718d7d3d0b
Felix <46941065+felix911104@users.noreply.github.com>
2019-07-10 00:37:57 -0700

    upload1

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Friend-Finder
+
+This Friend-Finder app is compatibility-based "FriendFinder" application -- basically a dating app.
+This full-stack site will take in results from users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+## Usage
+
+Visual Studio Code
+
+## Resource & Helps
+
+[Google](www.google.com)
+
+[w3school](https://www.w3schools.com)
+
+[stackoverflow](https://stackoverflow.com)
+
+## Github link  
+
+[GithubProfile](https://github.com/felix911104)
+
+## Repo links
+
+[bamazon](https://github.com/felix911104/Friend-Finder)
+
+## GoogleDrive Link to show the project 
+
+- not available
+
+## To use this app
+
+In order to test this app,
+install the npm packages list below.
+
+1. [express]
+2. [path]
+
+## Heroku link
+
+[Heroku](https://afternoon-headland-56642.herokuapp.com/)
+
+
+## Author
+Felix Zhang
---
+++ data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+        name: "Joey Tribbiani",
+        photo: "https://66.media.tumblr.com/4af2b3694af43138eea54e785133fe5e/tumblr_ot9bk9VuWX1w320ago4_250.jpg",
+        answers: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10
+        ]
+    },
+    {
+        name: "Chandler Bing",
+        photo: "https://www.tvovermind.com/wp-content/uploads/2017/04/Chandler-Bing.jpg",
+        answers: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10
+        ]
+    },
+    {
+        name: "Ross Geller",
+        photo: "https://img.buzzfeed.com/buzzfeed-static/static/2017-06/1/6/campaign_images/buzzfeed-prod-fastlane-01/how-popular-are-your-opinions-about-ross-geller-2-14231-1496313900-8_dblbig.jpg",
+        answers: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10
+        ]
+    },
+    {
+        name: "Monica Geller",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/0/0f/CourteneyCoxFeb09.jpg",
+        answers: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10
+        ]
+    },
+    {
+        name: "Rachel Green",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/a/ac/JenniferAniston08TIFF.jpg",
+        answers: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10
+        ]
+    },
+    {
+        name: "Phoebe Buffay",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Lisa_Kudrow_3.jpg",
+        answers: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10
+        ]
+    }
+]
+
+module.exports = friends;
